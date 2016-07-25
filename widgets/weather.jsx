9ea6e323e7871ecdc36d914243fe1757489158e7
Jon Friestedt + RB Embleton <jfrieste@gmail.com>
2016-07-25 18:05:48 -0400

    Finish weather

--- widgets/weather.jsx
+++ widgets/weather.jsx
@@ -3,12 +3,12 @@ const ReactDOM = require('react-dom');
 
 const Weather = React.createClass({
   getInitialState() {
-    return { time: new Date() };
+    return { time: new Date(), weather: {weather: [{description: 'loading'}], main: {temp: 'loading'}} };
   },
 
   componentDidMount() {
+    this.geo = navigator.geolocation.getCurrentPosition(this.handleLocation); // NB find current position navigator.geoposition
     this.interval = setInterval(this.tick, 1000);
-    this.geo = navigator.geolocation.getCurrentLocation(this.handleLocation); // NB find current position navigator.geoposition
   },
 
   tick() {
@@ -21,21 +21,23 @@ const Weather = React.createClass({
 
   handleLocation (pos) {
     const crd = pos.coords;
-    let url = "http://api.openweathermap.org/data/2.5/weather?lat={crd.latitude}&lon={crd.longitude}&appid=645c5d39c7603f17e23fcaffcea1a3c1";
+    let url = `http://api.openweathermap.org/data/2.5/weather?lat=${crd.latitude}&lon=${crd.longitude}&units=imperial&appid=645c5d39c7603f17e23fcaffcea1a3c1`;
     let request = new XMLHttpRequest();
-    request.open('GET', {url}, true);
-    let resp;
+    request.open('GET', url, true);
+    // debugger
 
     request.onload = function () {
       if (request.status >= 200 && request.status < 400) {
         // Success!
-        resp = request.responseText;
+        // debugger
+        this.state.weather = JSON.parse(request.responseText);
+        console.log("success");
+        // debugger
       } else {
         // Failure!
-        resp = undefined;
+        console.log("failure");
       }
-    };
-
+    }.bind(this);
     request.onerror = function () {
       // Error.
     };
@@ -50,9 +52,15 @@ const Weather = React.createClass({
   },
   render() {
     return(
-      <div className="clock">
-        <h1>Time: {this.state.time.toLocaleTimeString()}</h1>
-        <h1>Date: {this.state.time.toDateString()}</h1>
+      <div className="weatherclock">
+        <div className="clock">
+          <h1>Time: {this.state.time.toLocaleTimeString()}</h1>
+          <h1>Date: {this.state.time.toDateString()}</h1>
+        </div>
+        <div className="weather">
+          <h1>Weather: {this.state.weather.weather[0].description}</h1>
+          <h1>Temp: {this.state.weather.main.temp}</h1>
+        </div>
       </div>
 
     );
